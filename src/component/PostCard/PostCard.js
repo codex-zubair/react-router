6b4096118dce6cb59f2ff64d697786a773080d79
Rocko <codingwithrock@gmail.com>
2022-10-06 15:42:30 +0600

    added button for navigate things we can call it event handler with dinamically data load we can this use navigator and make navigate and go to desiare page also we can use link but i prepare to use navigator.

--- src/component/PostCard/PostCard.js
+++ src/component/PostCard/PostCard.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './PostCard.css'
 
-    const PostCard = (props) => {
+    const PostCard = ({post}) => {
 
-    const {body,title} = props.post;
+    const {body,title,id} = post;
+
+    const navigate = useNavigate(); 
+
+    const clicked = ()=> 
+    {
+        navigate(`/post/${id}`);
+    }
+
   
     return (
         <div className='post'>
             <h2>{title}</h2>
             <h4>{body}</h4>
+            <Link to={`/post/${id}`}><button>Show Details With Link</button></Link>
+            <button onClick={clicked}>Show Details</button>
         </div>
     );
 };
